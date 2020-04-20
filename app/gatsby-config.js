fec4b907da24c3146239a8291b79db51d0d99f05
Rômulo Bastos <romulobastos.design@gmail.com>
2020-04-19 22:40:46 -0300

    Add blog-post template and get all files from posts folder

--- app/gatsby-config.js
+++ app/gatsby-config.js
@@ -15,6 +15,17 @@ module.exports = {
 				path: `${__dirname}/src/images`,
 			},
 		},
+		{
+			resolve: `gatsby-source-filesystem`,
+			options: {
+				name: `posts`,
+				path: `${__dirname}/posts`,
+			},
+		},
+		{
+			resolve: `gatsby-transformer-remark`,
+			options: {},
+		},
 		`gatsby-transformer-sharp`,
 		`gatsby-plugin-sharp`,
 		{
